ccf6f5c77a028cb261f631ccf1ef0febf8eb0510
yorsh130204 <jorge130204@gmail.com>
2023-10-13 08:56:00 -0600

    Delete info from inputs

--- js/script.js
+++ js/script.js
@@ -13,6 +13,11 @@ document.getElementById('form')
     .then(() => {
       btn.value = 'Send Email';
       alert('Your email has been sent successfully!');
+      // Restablecer los valores de los campos de entrada
+      document.getElementById('to_name').value = '';
+      document.getElementById('email_id').value = '';
+      document.getElementById('from_name').value = '';
+      document.getElementById('message').value = '';
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
