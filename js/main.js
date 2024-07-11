93418bfe0d810feea75060014c65bf46365131dc
Jorge Alberto Valenzuela Castañón <134531815+yorsh130204@users.noreply.github.com>
2024-07-11 14:45:06 -0600

    Fixing client console.errors

--- js/main.js
+++ js/main.js
@@ -140,13 +140,6 @@
     });
   
     /**
-     * Initiate glightbox 
-     */
-    const glightbox = GLightbox({
-      selector: '.glightbox'
-    });
-  
-    /**
      * Porfolio isotope and filter
      */
     window.addEventListener('load', () => {
@@ -176,57 +169,43 @@
     });
   
     /**
-     * Initiate portfolio lightbox 
-     */
-    const portfolioLightbox = GLightbox({
-      selector: '.portfolio-lightbox'
-    });
-  
-    /**
      * Portfolio details slider
      */
-    new Swiper('.portfolio-details-slider', {
-      speed: 400,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      }
-    });
+    // new Swiper('.portfolio-details-slider', {
+    //   speed: 400,
+    //   loop: true,
+    //   autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false
+    //   },
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     type: 'bullets',
+    //     clickable: true
+    //   }
+    // });
   
     /**
      * Testimonials slider
      */
-    new Swiper('.testimonials-slider', {
-      speed: 600,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
-      slidesPerView: 'auto',
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      }
-    });
-  
-    /**
-     * Initiate galleery lightbox 
-     */
-    const galleeryLightbox = GLightbox({
-      selector: '.galleery-lightbox'
-    });
+    // new Swiper('.testimonials-slider', {
+    //   speed: 600,
+    //   loop: true,
+    //   autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false
+    //   },
+    //   slidesPerView: 'auto',
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //     type: 'bullets',
+    //     clickable: true
+    //   }
+    // });
   
     /**
      * Initiate Pure Counter 
      */
-    new PureCounter();
+    // new PureCounter();
   
   })()
